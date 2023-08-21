c551963dc55df0d951cfb98bd15c2f7c82d4361b
Corey Petty <corey@status.im>
2023-08-21 16:00:18 -0400

    config v4

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Logos Collective Project Roadmaps",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "roadmap.logos.co",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
