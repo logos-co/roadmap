db42e322d2504c59b85160aba00ab1002f9561c5
Florin Barbu <barbu_florin_adrian@yahoo.com>
2026-07-22 10:48:50 +0300

    ci: align build environments with Quartz 5

--- shell.nix
+++ shell.nix
@@ -1,4 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = with pkgs.buildPackages; [ nodejs_18 ];
+let
+  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  nixpkgs = lock.nodes.nixpkgs.locked;
+  pkgs = import
+    (builtins.fetchTarball {
+      inherit (nixpkgs) url;
+      sha256 = nixpkgs.narHash;
+    })
+    { };
+in
+pkgs.mkShellNoCC {
+  packages = with pkgs.buildPackages; [ nodejs_22 ];
 }
